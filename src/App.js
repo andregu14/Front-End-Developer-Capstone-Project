1c57a6387f0d5856fc1e2fa827f7a6763210a751
André Gustavo <andregustavo147@gmail.com>
2023-07-18 03:42:59 -0300

    Cleanup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { ChakraProvider } from "@chakra-ui/react";
 import Aside from './assets/components/Aside';
 import Main from './assets/components/Main';
-import Basic from './assets/components/Basic';
 
 function App() {
   return (
