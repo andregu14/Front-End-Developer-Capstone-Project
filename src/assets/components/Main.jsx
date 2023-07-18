050618c690a37bc17f9a442137f75869d7eea124
André Gustavo <andregustavo147@gmail.com>
2023-07-18 03:43:57 -0300

    Update Main.jsx
    Using chakra ui and react-icons update

--- src/assets/components/Main.jsx
+++ src/assets/components/Main.jsx
@@ -1,26 +1,38 @@
 import { useFormik } from "formik";
 import { useState } from "react";
 import * as Yup from "yup";
+import { AiOutlineArrowLeft } from 'react-icons/ai';
 import {
     FormControl,
     FormLabel,
     FormErrorMessage,
-    FormHelperText,
     Button,
     NumberInput,
     NumberInputField,
     NumberInputStepper,
     NumberIncrementStepper,
     NumberDecrementStepper,
+    Select,
+    Input,
+    Box,
+    Wrap,
+    WrapItem,
+    Heading,
   } from '@chakra-ui/react'
 
 function Main() {
+    const [resDate, setResDate] = useState("")
+    const [resTime, setResTime] = useState("")
+    const [guests, setGuests] = useState(2)
+    const [occasion, setOccasion] = useState("")
+    const [availableTimes, setAvailableTimes] = useState(["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"])
+
     const formik = useFormik({
         initialValues: {
-            resDate: "",
-            resTime: "",
-            guests: 2,
-            occasion: "",
+            resDate: resDate,
+            resTime: resTime,
+            guests: guests,
+            occasion: occasion,
         },
         onSubmit: (e) => {
             e.preventDefault();
@@ -28,93 +40,96 @@ function Main() {
         validationSchema: Yup.object({
             resDate: Yup.date().required('Required'),
             resTime: Yup.string().required('Required'),
-            guests: Yup.number().required('Required').min(1, 'The minimum number is 1' ).max(10, 'The maximum number is 10'),
+            guests: Yup.number().required('Required'),
             occasion: Yup.string().required('Required'),
           }),
       });
   
     return (
     <main>
-        <header>
-            <button className="back-button">
-                <span class="material-symbols-outlined">
-                    arrow_left_alt
-                </span>
-            </button>
-            <h1>Book a Table</h1>
-        </header>
-
-        <form onSubmit={formik.handleSubmit} 
-        style={{
-        display: "grid",
-        maxWidth: "200px",
-        gap: "15px",
-        }}>
-            <FormControl isRequired isInvalid={formik.touched.resDate && formik.errors.resDate}>
-                <FormLabel htmlFor="resDate">Choose date</FormLabel>
-                <input
-                    type="date"
-                    id="resDate"
-                    name="resDate"
-                    {...formik.getFieldProps('resDate')}
-                />
-                <FormErrorMessage>{formik.errors.resDate}</FormErrorMessage>
-            </FormControl>
-        
-            <FormControl isRequired isInvalid={formik.errors.resTime}>
-                <FormLabel htmlFor="resTime">Choose time</FormLabel>
-                <select
-                    id="resTime"
-                    name="resTime"
-                    {...formik.getFieldProps('resTime')}
-                >   
-                    <option>17:00</option>
-                    <option>18:00</option>
-                    <option>19:00</option>
-                    <option>20:00</option>
-                    <option>21:00</option>
-                    <option>22:00</option>
-                </select>
-                <FormErrorMessage>{formik.errors.resTime}</FormErrorMessage>
-            </FormControl>
-
-            <FormControl isRequired isInvalid={formik.errors.guests}>
-                <FormLabel htmlFor="guests">Number of guests</FormLabel>
-                    <NumberInput 
-                        name="guests"
-                        id="guests"
-                        max={10} min={1}
+        <Box>
+            <Wrap spacing='135px' mb={10}>
+                <WrapItem>
+                    <Button colorScheme="green">{<AiOutlineArrowLeft />}</Button>
+                </WrapItem>
+                <WrapItem>
+                    <Heading as="h1">Book a Table</Heading>
+                </WrapItem>
+            </Wrap>
+            
+            <form onSubmit={formik.handleSubmit}
+            style={{
+            display: "grid",
+            maxWidth: "225px",
+            gap: "15px",
+            }}>
+                <FormControl isRequired>
+                    <FormLabel htmlFor="resDate">Choose date</FormLabel>
+                    <Input
+                        type="date"
+                        id="resDate"
+                        name="resDate"
+                        value={resDate}
+                        onChange={e => setResDate(e.target.value)}
+                    />
+                    <FormErrorMessage>{formik.errors.resDate}</FormErrorMessage>
+                </FormControl>
+            
+                <FormControl isRequired>
+                    <FormLabel htmlFor="resTime">Choose time</FormLabel>
+                    <Select
+                        id="resTime"
+                        name="resTime"
+                        onChange={e => setResTime(e.target.value)}
+                        placeholder="Select the Time"
                     >
-                        <NumberInputField />
-                        <NumberInputStepper>
-                            <NumberIncrementStepper />
-                            <NumberDecrementStepper />
-                        </NumberInputStepper>
-                    </NumberInput>
-                <FormErrorMessage>{formik.errors.guests}</FormErrorMessage>
-            </FormControl> 
-
-            <FormControl isRequired isInvalid={formik.errors.occasion}>
-                <FormLabel htmlFor="occasion">Occasion</FormLabel>       
-                    <select
-                        id="occasion"
-                        name="occasion"
-                        {...formik.getFieldProps('occasion')}
-                    >
-                        <option>Birthday</option>
-                        <option>Anniversary</option>
-                    </select>
-                <FormErrorMessage>{formik.errors.occasion}</FormErrorMessage>
-            </FormControl>
-
-            <Button
-                mt={4}
-                colorScheme='teal'
-                type='submit'
-            >
-                Submit
-            </Button>
-        </form>
+                        {availableTimes.map(time => (
+                            <option key={time}>{time}</option>
+                        ))}
+                    </Select>
+                    <FormErrorMessage>{formik.errors.resTime}</FormErrorMessage>
+                </FormControl>
+                <FormControl isRequired isInvalid={formik.touched.guests && formik.errors.guests}>
+                    <FormLabel htmlFor="guests">Number of guests</FormLabel>
+                        <NumberInput
+                            name="guests"
+                            id="guests"
+                            max={10} min={1}
+                            defaultValue={guests}
+                            onChange={(value) => setGuests(value)}
+                        >
+                            <NumberInputField/>
+                            <NumberInputStepper>
+                                <NumberIncrementStepper />
+                                <NumberDecrementStepper />
+                            </NumberInputStepper>
+                        </NumberInput>
+                    <FormErrorMessage>{formik.errors.guests}</FormErrorMessage>
+                </FormControl>
+                <FormControl isRequired >
+                    <FormLabel htmlFor="occasion">Occasion</FormLabel>
+                        <Select
+                            id="occasion"
+                            name="occasion"
+                            placeholder="Select the Occasion"
+                            onChange={e => setOccasion(e.target.value)}
+                        >
+                            <option>Birthday</option>
+                            <option>Anniversary</option>
+                        </Select>
+                    <FormErrorMessage>{formik.errors.occasion}</FormErrorMessage>
+                </FormControl>
+                <Button
+                    mt={4}
+                    colorScheme='teal'
+                    type='submit'
+                    bg={"#F4CE14"}
+                    color={'black'}
+                >
+                    Submit
+                </Button>
+            </form>
+        </Box>
     </main>
 )}
   
